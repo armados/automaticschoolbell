fe35477cce3d793e85eb08b62f57ebbb99aa8d17
Nikos Zounis <armados@gmail.com>
2018-04-18 22:53:54 +0300

    Add quotes

---
+++ src/epalquotes.py
@@ -0,0 +1,34 @@
+import os
+import random
+from pathlib import Path 
+
+
+def fetchQuotesFromFile():
+
+    quotefile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'quotes.txt')
+
+    if Path(quotefile).is_file() == False:
+        return None
+
+    fd = open(quotefile, 'r')
+    
+    quotes = fd.readlines()
+
+    fd.close()
+
+    quotes = [line.strip() for line in quotes if line.strip()]
+
+    return quotes
+
+
+
+def getRandomQuote():
+    quotes = fetchQuotesFromFile()
+    
+    if quotes == None:
+        return None
+
+    return random.choice(quotes)
+
+
+print (getRandomQuote())
